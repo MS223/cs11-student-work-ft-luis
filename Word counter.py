38229793ee4aec2c3b9e2b3c271babd43d6a1d25
ft-luis <luis.figueroa@ms223.org>
2016-05-17 09:29:00 -0400

    Create Word counter.py

---
+++ Word counter.py
@@ -0,0 +1,9 @@
+text_input = raw_input
+
+def word_count(word):
+    word_count = []
+    word = word.lower().split()
+    for word in word_count():
+        if word in word_count():
+            word_count(word)
+            
